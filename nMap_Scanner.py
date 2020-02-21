b64f2e312766e1ce70ebf0737bb93892b2fc7720
Sagar Makar <46819032+sagarmakar@users.noreply.github.com>
2020-02-21 10:32:42 +0530

    Update nMap_Scanner.py

--- nMap_Scanner.py
+++ nMap_Scanner.py
@@ -6,7 +6,7 @@ def ns1():
   print('All hosts: ' + ns.all_hosts())
   ip= str(input('input your ip: ')
 def start():
-  in1= int(input('Hello! select the action: /n 1. state of ip /n  2. protocol available /n 3. key of protocol /n 4. see the open port'))
+  in1= int(input('Hello! select the action: /n 1. state of ip /n  2. protocol available /n 3. key of protocol /n 4. see the open port /n 5. see scan results.'))
   if in1= 1:
     ns1()
     print(ns[ip].state())
@@ -25,6 +25,10 @@ def start():
     in2= int(input('port to be checked: ')
     print(ns[ip].has_tcp(in2))
              
+  elif in1= 5:
+    in2= int(input('scan range: '))
+    ns.scan(ip, in2)
+    ns.command_line
   else:
     print('Please select a valid option.')
     ns1()
